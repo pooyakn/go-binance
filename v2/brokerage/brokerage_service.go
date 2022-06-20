47084a0cb431dc83523587aeb2f6bee8d8b044bb
Pooya Kermaninezhad <pooya.kn.73@gmail.com>
2022-06-20 19:19:51 +0300

    Fix brokerage confirm time

--- v2/brokerage/brokerage_service.go
+++ v2/brokerage/brokerage_service.go
@@ -301,7 +301,7 @@ type GetSubAccountDepositHistoryResponse struct {
 	Status        int    `json:"status"`
 	TxID          string `json:"txId"`
 	SourceAddress string `json:"sourceAddress"`
-	ConfirmTimes  string `json:"confirmTimes"`
+	ConfirmTimes  string `json:"confirmNo"`
 }
 
 type GetSubAccountTransferHistoryService struct {
